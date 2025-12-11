b1e832d6fd805f85bfa8d7d672e40de8b24ee149
Zyczzz1 <zralfonso@paterostechnologicalcollege.edu.ph>
2025-12-11 19:19:46 +0800

    Delete script.js

--- script.js
+++
@@ -1,4 +0,0 @@
-document.getElementById("btn").addEventListener("click", function () {
-    const message = document.getElementById("message");
-    message.textContent = "Have a great day! 😊";
-});
